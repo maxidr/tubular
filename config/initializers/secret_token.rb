4b92d882787a07c960610f0d2b51305867a90150
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-07-12 10:11:46 -0300

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tubular::Application.config.secret_token = '7d2eab0ba2c2c821e056c90744a9dd2ac1719d8d77a7aa7ec40b8938f25af1f0b091da855c4ebc003852c36cda808d7a79913cc75ceb7364abd42f931a038617'
