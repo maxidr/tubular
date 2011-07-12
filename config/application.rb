90630e1ccd06692381c0fe18e8a9ed4553292c92
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-07-12 14:53:09 -0300

    Se agrega jquery a las librerias cargadas

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Tubular
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w()
+    config.action_view.javascript_expansions[:defaults] = %w(jquery.min)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -40,3 +40,4 @@ module Tubular
     config.filter_parameters += [:password]
   end
 end
+
