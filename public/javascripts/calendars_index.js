d078e99546ed2ca21097558e0c133152d5de6e4b
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-09-16 12:54:50 -0300

    Se agrega el bloqueo de pantalla cuando se envía la petición ajax de info para el schedule, en la pantalla del calendario.

--- public/javascripts/calendars_index.js
+++ public/javascripts/calendars_index.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {	
+    $.blockUI.defaults.css = {};
+    $.blockUI.defaults.message = "Aguarde por favor...";
     
 		$('#calendar').fullCalendar({
 			editable: true,
 			events: '/calendars/events',
-      eventClick: function(calEvent, jsEvent, view) {
-        // Schedule ID        
+      eventClick: function(calEvent, jsEvent, view) {        
+        $("#calendar").block();
+        // Schedule ID
         $.ajax({
           url: 'schedules/' + calEvent.id,
           dataType: 'script'
-        });
+        }).complete(function() { $("#calendar").unblock() });
       }
 		});
 		
